433336a8a35bccdd3ac0c366cc513bae1eb059c6
heekyoung2000 <heekyoung2000@naver.com>
2023-05-24 06:29:39 +0000

    tiny 연습문제 추가

--- tiny/cgi-bin/head-adder.c
+++ tiny/cgi-bin/head-adder.c
@@ -2,27 +2,41 @@
 
 
 int main(void) {
-  char *buf, *p, *arg1_p, *arg2_p, *method;
-  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE], val1[MAXLINE], val2[MAXLINE];
-  int n1=0, n2=0;
+  // char *buf, *p, *arg1_p, *arg2_p, *method;
+  // char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE], val1[MAXLINE], val2[MAXLINE];
+  // int n1=0, n2=0;
 
   /*Extract the two argument*/
-  if ((buf = getenv("QUERY_STRING")) != NULL) {
-    p = strchr(buf, '&');
-    *p = '\0';
-    strcpy(arg1, buf);
-    strcpy(arg2, p+1);
-    // fnum=value에서 value만 추출하기 위한 작업
-    arg1_p = strchr(arg1, '=');
-    *arg1_p = '\0';
-    strcpy(val1, arg1_p+1);
-    // snum=value에서 value만 추출하기 위한 작업
-    arg2_p = strchr(arg2, '=');
-    *arg2_p = '\0';
-    strcpy(val2, arg2_p+1);
-    //문자를 정수로 전환하는 atoi함수
-    n1 = atoi(val1);
-    n2 = atoi(val2);
+  // if ((buf = getenv("QUERY_STRING")) != NULL) {
+  //   p = strchr(buf, '&');
+  //   *p = '\0';
+  //   strcpy(arg1, buf);
+  //   strcpy(arg2, p+1);
+  //   // fnum=value에서 value만 추출하기 위한 작업
+  //   arg1_p = strchr(arg1, '=');
+  //   *arg1_p = '\0';
+  //   strcpy(val1, arg1_p+1);
+  //   // snum=value에서 value만 추출하기 위한 작업
+  //   arg2_p = strchr(arg2, '=');
+  //   *arg2_p = '\0';
+  //   strcpy(val2, arg2_p+1);
+  //   //문자를 정수로 전환하는 atoi함수
+  //   n1 = atoi(val1);
+  //   n2 = atoi(val2);
+  // }
+
+  char *buf, *p, *method;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  int n1=0, n2=0;
+
+  /* 자식 프로세스가 따로 만든 QUERT_STRING 환경변수를 getenv로 가져와 buf에 넣음 */
+  if((buf = getenv("QUERY_STRING")) != NULL){
+      p = strchr(buf, '&'); // 포인터 p는 &가 있는 곳의 주소
+      *p = '\0';      // &를 \0로 바꿔주고
+      strcpy(arg1, buf);  // & 앞에 있었던 인자
+      strcpy(arg2, p+1);  // & 뒤에 있었던 인자
+      n1 = atoi(arg1);
+      n2 = atoi(arg2);
   }
 
   method = getenv("REQUEST_METHOD");
